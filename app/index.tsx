0d2e3d10e107ddfdb68df8f4f673d6b7ef19518e
Alex <alex.edmiston80@gmail.com>
2025-02-10 16:21:03 +0100

    Parte-alex

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity} from 'react-native'
+import { View, Text, TouchableOpacity,StyleSheet,Image} from 'react-native'
 import {Link} from "expo-router";
 import React from 'react'
 
@@ -6,15 +6,66 @@ import React from 'react'
 
 const app = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
+    <View style={estilos.contenedor}>
+      <Image source={require("../assets/images/fondo.png")} style={estilos.fondo} />
+      <View style={estilos.textoContainer}>
+      <Text style={estilos.texto}>Bienvenido</Text>
+      </View>
+      <View style={estilos.cajaBlanca}>
         <TouchableOpacity>
-          <Link href={"/autentificacion"}>Autentificacion</Link>
-          <Link href={"/listado"}>Listado</Link>
+          <Link href={"/autentificacion"} style={estilos.textoCaja}>Acceder</Link>
         </TouchableOpacity>
+      </View>
+      <Link href={"/listado"} style={{color:"red"}}>Listado</Link>
     </View>
   )
 }
+const estilos = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cajaBlanca: {
+    zIndex: 1,
+    backgroundColor: '#F9F9F9',
+    width: '25%', 
+    height: '22%', 
+    borderRadius:20,
+    borderWidth:4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:90,
+    top: '10%',
+  },
+  textoCaja: {
+    color: 'black',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  fondo: {
+    position: 'absolute',
+    width: '100%',
+    height: '200%',
+  },
+  texto: {
+    color: 'white',
+    fontSize: 44,
+    fontWeight: 'bold',
+    textShadowColor: 'black',
+    textShadowRadius: 1,
+    textShadowOffset: { 
+      width: 2,
+      height: 2,
+    },
+  },
+  textoContainer: {
+    zIndex: 1, 
+    marginBottom:20,
+    textAlign: 'center',
+  },
+});
 
 
 export default app
