7cb6a5ccf88e16b2099d7ca83d1aaece16c5bf17
Paucher22 <4247636@alu.murciaeduca.es>
2025-02-12 13:58:54 +0100

    bienmal1

--- app/auth_bien.tsx
+++ app/auth_bien.tsx
@@ -1,14 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { Link } from 'expo-router'
 
+const Bien = () => {
 
-const bien = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
-  )
-}
+    <ImageBackground
+    source={require("../assets/images/fondo.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>LECTURA CORRECTA</Text>
+        <Link href="/listado">
+        <Image
+          source={require('../assets/images/check.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        </Link>
+      </View>
+    </ImageBackground>
+  );
+};
 
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',     
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
 
-export default bien
+export default Bien;
--- app/auth_mal.tsx
+++ app/auth_mal.tsx
@@ -1,14 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { Link } from 'expo-router'
 
 const mal = () => {
+
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
-  )
+    <ImageBackground
+      source={require("../assets/images/fondo.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>ERROR DE VALIDACION</Text>
+        <Text style={styles.title}>INTENTELO DE NUEVO</Text>
+        <Link href="/autentificacion">
+        <Image
+          source={require('../assets/images/cross.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        </Link>
+      </View>
+    </ImageBackground>
+  );
 }
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',     
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
 
 
 export default mal
